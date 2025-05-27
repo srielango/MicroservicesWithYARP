4b81465dfbf68f5e4ee66f3fce53ac85d0e62685
srielango <srielango@gmail.com>
2025-05-27 22:59:25 +0530

    Added YARP gateway for catalog service

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,5 +1,4 @@
 using CatalogAPI.Models;
-using Microsoft.AspNetCore.Builder;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,20 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
 
 var products = LoadProducts();
 
